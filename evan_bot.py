f22baff04ee6aa90cd9da57a53df47961131644d
BuildTools <unconfigured@null.spigotmc.org>
2021-07-05 15:21:02 -0700

    test question and record answers

--- evan_bot.py
+++ evan_bot.py
@@ -2,28 +2,48 @@ import requests
 import json
 from discord.ext import commands
 
+# constants
 cp = '!'
+test_question = "This is a test"
 
+answers = dict()
+is_q_active = False
+
+
+# step up stuff
 client = commands.Bot(command_prefix= cp)
 
-client.command_prefix = cp # Setting the prefix of the bot to reflect everywhere it is mentioed
+client.command_prefix = cp
 
 
+# records responses
 @client.event
 async def on_message(message):
     await client.process_commands(message)
 
-    text = str(message.content)
+    name = str(message.author)
+    msg = str(message.content)
 
-    if not (message.author == client.user) and not(client.command_prefix == text[0]):
-        await message.channel.send("wow a message!")
+    if is_q_active == True:
+        answers[name] = msg.lower()
     
 
-
+# sends a question to the chat
 @client.command()
-async def hi(ctx):
-    await ctx.send("hello")
+async def question(ctx):
+    await ctx.send(test_question)
+    global is_q_active
+    is_q_active = True
 
+# ends answering period
+@client.command()
+async def end(ctx):
+    global is_q_active
+    is_q_active = False
+    print(answers)
 
-# run the client on the server
-client.run('ODU1MjA4OTAzNDY0NTgzMTY4.YMvJWw.Fs19c3W1QAPAd5jfXCTfCw8QPZw')
+# runs the client on the server
+with open('token.json') as json_data:
+    data = json.load(json_data)
+    token = data["token"]
+client.run(token)
