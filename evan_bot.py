082313c434743d6273e2c88855b5f9f4cd1139f0
BuildTools <unconfigured@null.spigotmc.org>
2021-07-05 16:16:26 -0700

    added next function

--- evan_bot.py
+++ evan_bot.py
@@ -2,15 +2,15 @@ import requests
 import json
 from discord.ext import commands
 
-# constants
-cp = '!'
+# variables
+cp = "!"
 test_question = "This is a test"
 
 answers = dict()
 is_q_active = False
 
 
-# step up stuff
+# set up bot
 client = commands.Bot(command_prefix= cp)
 
 client.command_prefix = cp
@@ -24,7 +24,7 @@ async def on_message(message):
     name = str(message.author)
     msg = str(message.content)
 
-    if is_q_active == True:
+    if is_q_active:
         answers[name] = msg.lower()
     
 
@@ -35,6 +35,7 @@ async def question(ctx):
     global is_q_active
     is_q_active = True
 
+
 # ends answering period
 @client.command()
 async def end(ctx):
@@ -42,8 +43,13 @@ async def end(ctx):
     is_q_active = False
     print(answers)
 
+@client.command()
+async def next(ctx):
+    await question(ctx)
+
+
 # runs the client on the server
-with open('token.json') as json_data:
+with open("token.json") as json_data:
     data = json.load(json_data)
     token = data["token"]
 client.run(token)
