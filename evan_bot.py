454bf04318ff3393faefae343c7c4599ec401c38
BuildTools <unconfigured@null.spigotmc.org>
2021-07-07 17:59:33 -0700

    added evaluation of answers

--- evan_bot.py
+++ evan_bot.py
@@ -1,5 +1,6 @@
-import requests
 import json
+import random
+from bs4 import BeautifulSoup
 from discord.ext import commands
 from QuestionHelper import QuestionHelper
 from Question import Question # !!! is there a better way to import these?
@@ -27,12 +28,15 @@ async def on_message(message):
     await client.process_commands(message)
     name = str(message.author)
     msg = str(message.content)
+    msg = msg.lower()
+    msg = msg.strip()
     if msg[0] == "!":
         return
 
     if is_q_active:
-        answers[name] = msg.lower()
+        answers[name] = msg
     
+
 # sets up category and stuff
 @client.command()
 async def setup(ctx, num_questions, category):
@@ -53,13 +57,37 @@ async def setup(ctx, num_questions, category):
 
 
 # sends a question to the chat
+# !!! dif bwt tf and ma
+# !!! check for last question
 @client.command()
 async def question(ctx):
     global question
+
+    if len(game.question_queue) == 0:
+        await ctx.send("The question queue is empty")
+        return
+
     question = game.get_next_question()
-    await ctx.send(question.question)
+    q = BeautifulSoup(question.question, "lxml").get_text()
+    
+    if question.type == "boolean":
+        await ctx.send(q)
+    
+    else:
+        a = BeautifulSoup(get_ans_choices(question), "lxml").get_text()
+        msg = q + "\n" + a
+        await ctx.send(msg)
+    
     global is_q_active
     is_q_active = True
+
+def get_ans_choices(q: Question) -> str:
+    ans = q.incorrect_answers
+    x = random.randrange(0,len(ans))
+    ans.insert(x, q.correct_answer)
+    msg = format_list(ans, "or")
+
+    return msg
 
 
 # ends answering period
@@ -67,12 +95,52 @@ async def question(ctx):
 async def end(ctx):
     global is_q_active
     is_q_active = False
-    print(answers)
 
-# sends scores
+    ans = question.correct_answer
+    correct = list_correct_users(ans)
+    correct = strip_username(correct)
+    ppl = format_list(correct, "and")
+    if ppl == "":
+        ppl = "Nobody"
+        
+    await ctx.send(f"The correct answer was {ans}\n{ppl} got it right!")
+
+
+def list_correct_users(ans) -> list:
+    ans = ans.lower()
+    ans = ans.strip()
+    correct = []
+
+    for user, answer in answers.items():
+        if answer == ans:
+            correct.append(user)
+    return correct
+
+# !!!sends scores
 @client.command()
 async def scores():
     return
+
+
+#helper functions
+
+# turns list into a string with the given conjunction
+def format_list(lst: list, conj: str) -> str:
+    if len(lst) == 1:
+        return str(lst[0])
+    else: 
+        string = ""
+        for i in range(len(lst)):
+            if i != (len(lst) - 1):
+                string = string + lst[i] + ", "
+            else:
+                string = string + conj + " " + lst[i]
+        return string
+
+# strips numbers from username
+def strip_username(users: list) -> list:
+    striped_users = [user.split('#')[0] for user in users]
+    return striped_users
 
 
 # run the client on the server
@@ -80,3 +148,5 @@ with open("token.json") as json_data:
     data = json.load(json_data)
     token = data["token"]
 client.run(token)
+
+
